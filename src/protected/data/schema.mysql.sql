2cb00b6215d6dbc0a30949a96a7ef760418e0779
Knut Urdalen <knut.urdalen@gmail.com>
2011-08-16 08:00:16 +0200

    updated mysql schema

--- src/protected/data/schema.mysql.sql
+++ src/protected/data/schema.mysql.sql
@@ -1,57 +1,53 @@
-CREATE TABLE IF NOT EXISTS `forum` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `position` int(11) NOT NULL DEFAULT '0',
-  `created_at` datetime NOT NULL,
-  `modified_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS forum (
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  description TEXT NOT NULL,
+  position INTEGER NOT NULL DEFAULT 0,
+  created_at DATETIME NOT NULL,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `post` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `topic_id` int(10) unsigned NOT NULL,
-  `forum_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS post (
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `user_id` INTEGER NOT NULL,
+  `topic_id` INTEGER NOT NULL,
+  `forum_id` INTEGER NOT NULL,
   `body` text NOT NULL,
   `body_html` text NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `created_at` DATETIME NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `topic` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `forum_id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `hits` int(11) NOT NULL,
-  `sticky` tinyint(1) NOT NULL,
-  `locked` tinyint(1) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `forum_id` INTEGER NOT NULL,
+  `user_id` INTEGER NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `hits` INTEGER NOT NULL DEFAULT '0',
+  `sticky` tinyint(1) NOT NULL DEFAULT '0',
+  `locked` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` DATETIME NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password_hash` varchar(32) NOT NULL,
   `admin` tinyint(1) NOT NULL,
-  `display_name` varchar(100) NOT NULL,
-  `website` varchar(255) NOT NULL,
+  `display_name` varchar(100) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
   `login_key` varchar(32) NOT NULL,
   `login_key_expires_at` datetime NOT NULL,
   `activated` tinyint(1) NOT NULL,
-  `bio` text NOT NULL,
-  `bio_html` text NOT NULL,
-  `openid_url` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `last_login_at` datetime NOT NULL,
-  `last_seen_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `bio` text DEFAULT NULL,
+  `bio_html` text DEFAULT NULL,
+  `openid_url` varchar(255) DEFAULT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `last_login_at` DATETIME DEFAULT NULL,
+  `last_seen_at` DATETIME DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-
+INSERT INTO forum(id, name, description, position, created_at) VALUES(1, 'General', 'General Discussion goes here', 1, CURRENT_TIMESTAMP);
