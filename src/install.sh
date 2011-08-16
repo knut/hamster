7a4e36871c035ad92649c6995cf7faac7c80eafb
Knut Urdalen <knut.urdalen@gmail.com>
2011-08-16 08:13:56 +0200

    updated from using Yii Framework 1.1.2 to 1.1.8

--- src/install.sh
+++ src/install.sh
@@ -2,6 +2,6 @@
 set -x
 set -e
 
-echo "Downloading Yii Framework 1.1.2"
+echo "Downloading Yii Framework 1.1.8"
 cd protected/lib
-svn export http://yii.googlecode.com/svn/tags/1.1.2/framework yii
+svn export http://yii.googlecode.com/svn/tags/1.1.8/framework yii
