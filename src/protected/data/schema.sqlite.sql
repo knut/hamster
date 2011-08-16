8b9e676ef36f480d93e833a31617532f13f3a834
Knut Urdalen <knut.urdalen@gmail.com>
2011-08-16 07:54:50 +0200

    fixed sqlite schema

--- src/protected/data/schema.sqlite.sql
+++ src/protected/data/schema.sqlite.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS forum (
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  name VARCHAR(100) NOT NULL,
+  name TEXT NOT NULL,
   description TEXT NOT NULL,
   position INTEGER NOT NULL DEFAULT 0,
   created_at DATETIME NOT NULL,
-  updated_at DATETIME NOT NULL DEFAULT (DATETIME('now'))
+  updated_at DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS post (
@@ -12,46 +12,47 @@ CREATE TABLE IF NOT EXISTS post (
   `user_id` INTEGER NOT NULL,
   `topic_id` INTEGER NOT NULL,
   `forum_id` INTEGER NOT NULL,
-  `body` text NOT NULL,
-  `body_html` text NOT NULL,
+  `body` TEXT NOT NULL,
+  `body_html` TEXT NOT NULL,
   `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL DEFAULT (DATETIME('now'))
+  `updated_at` DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `topic` (
   `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `forum_id` INTEGER NOT NULL,
   `user_id` INTEGER NOT NULL,
-  `title` VARCHAR(100) NOT NULL,
+  `title` TEXT NOT NULL,
   `hits` INTEGER NOT NULL DEFAULT '0',
-  `sticky` tinyint(1) NOT NULL DEFAULT '0',
-  `locked` tinyint(1) NOT NULL DEFAULT '0',
+  `sticky` TINYINT(1) NOT NULL DEFAULT '0',
+  `locked` TINYINT(1) NOT NULL DEFAULT '0',
   `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL DEFAULT (DATETIME('now'))
+  `updated_at` DATETIME NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password_hash` varchar(32) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  `display_name` varchar(100) DEFAULT NULL,
-  `website` varchar(255) DEFAULT NULL,
-  `login_key` varchar(32) NOT NULL,
-  `login_key_expires_at` datetime NOT NULL,
-  `activated` tinyint(1) NOT NULL,
-  `bio` text DEFAULT NULL,
-  `bio_html` text DEFAULT NULL,
-  `openid_url` varchar(255) DEFAULT NULL,
+  `username` TEXT NOT NULL,
+  `email` TEXT NOT NULL,
+  `password_hash` TEXT NOT NULL,
+  `admin` TINYINT(1) NOT NULL,
+  `display_name` TEXT,
+  `website` TEXT,
+  `login_key` TEXT NOT NULL,
+  `login_key_expires_at` DATETIME NOT NULL,
+  `activated` TINYINT(1) NOT NULL,
+  `bio` TEXT,
+  `bio_html` TEXT,
+  `openid_url` TEXT,
   `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL DEFAULT (DATETIME('now')),
-  `last_login_at` DATETIME DEFAULT NULL,
-  `last_seen_at` DATETIME DEFAULT NULL
+  `updated_at` DATETIME NOT NULL,
+  `last_login_at` DATETIME,
+  `last_seen_at` DATETIME
 );
 
-
-INSERT INTO forum(id, name, description, position, created_at, updated_at) VALUES('1', 'General', 'General Discussion goes here', '1', DATETIME('now'), DATETIME('now'));
+-- Add one line per forum you want to have here
+INSERT INTO forum(id, name, description, position, created_at, updated_at) VALUES(1, 'General', 'General discussion goes here', 1, DATETIME('now'), DATETIME('now'));
+INSERT INTO forum(id, name, description, position, created_at, updated_at) VALUES(2, 'Off-topic', 'Off-topic discussion goes here', 1, DATETIME('now'), DATETIME('now'));
 
 
 
