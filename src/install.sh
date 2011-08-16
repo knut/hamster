168c96d07585d36020b9301bf39517e0004b53b1
Emil Haukeland <emil.haukeland@gmail.com>
2011-08-16 23:36:01 +0200

    Added a conditional mkdir for protected/lib

--- src/install.sh
+++ src/install.sh
@@ -3,5 +3,11 @@ set -x
 set -e
 
 echo "Downloading Yii Framework 1.1.8"
+
+# Create the directory if it doesn't exist already
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir -p protected/lib
+fi
+
 cd protected/lib
 svn export http://yii.googlecode.com/svn/tags/1.1.8/framework yii
