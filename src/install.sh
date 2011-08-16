07a0a4c0f2fbe7d77b5c13bbccd935335dd4161b
Emil Haukeland <emil.haukeland@gmail.com>
2011-08-17 00:31:55 +0200

    Added conditional mkdirs for protected/runtime and assets. Also made sure whey'll be writeable

--- src/install.sh
+++ src/install.sh
@@ -4,9 +4,21 @@ set -e
 
 echo "Downloading Yii Framework 1.1.8"
 
-# Create the directory if it doesn't exist already
-if [ ! -d "$DIRECTORY" ]; then
+# Create the lib directory if it doesn't exist already
+if [ ! -d "protected/lib" ]; then
 	mkdir -p protected/lib
+fi
+
+# Create the runtime directory if it doesn't exist already and make sure it's writeable
+if [ ! -d "protected/runtime" ]; then
+	mkdir -p protected/runtime
+	chmod 777 protected/runtime
+fi
+
+# Create the assets directory if it doesn't exist already and make sure it's writeable
+if [ ! -d "" ]; then
+	mkdir -p assets
+	chmod 777 assets
 fi
 
 cd protected/lib
